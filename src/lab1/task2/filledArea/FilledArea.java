9c414248ca898fa090afe6712cbecd4ac3a110d6
Damir Abdulin <damir.abdulin@tutanota.com>
2022-09-29 10:13:07 +0300

    [fix] Rename class

--- src/lab1/task2/filledArea/FilledArea.java
+++ src/lab1/task2/filledArea/FilledArea.java
@@ -20,7 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package lab1.task2.fieldArea;
+package lab1.task2.filledArea;
 
-public class FieldArea {
+public class FilledArea {
 }
